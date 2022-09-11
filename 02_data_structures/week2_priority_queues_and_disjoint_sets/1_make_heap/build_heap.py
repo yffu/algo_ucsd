89636954371321d2c51e446026639c3a9e190d11
yffu <yffu@umich.edu>
2022-09-10 21:32:23 -0700

    week2 problem1 solution

---
+++ 02_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -0,0 +1,60 @@
+# python3
+
+debug = False
+
+
+def sift_down(index, data, swaps):
+    m_index = index
+    l_child = index * 2 + 1
+    r_child = index * 2 + 2
+    if debug:
+        print('l_child i: ' + str(l_child))
+        print('r_child i: ' + str(r_child))
+    if l_child < len(data) and data[l_child] < data[m_index]:
+        m_index = l_child
+    if r_child < len(data) and data[r_child] < data[m_index]:
+        m_index = r_child
+    if debug:
+        print('m_index: ' + str(m_index) + ' index: ' + str(index))
+    if m_index != index:
+        data[index], data[m_index] = data[m_index], data[index]
+        swaps.append([index, m_index])
+        sift_down(m_index, data, swaps)
+
+
+def build_heap(data):
+    """Build a heap from ``data`` inplace.
+
+    Returns a sequence of swaps performed by the algorithm.
+    """
+    # The following naive implementation just sorts the given sequence
+    # using selection sort algorithm and saves the resulting sequence
+    # of swaps. This turns the given array into a heap, but in the worst
+    # case gives a quadratic number of swaps.
+    #
+    # TODO: replace by a more efficient implementation
+    swaps = []
+    n = len(data)//2
+
+    while n:
+        if debug:
+            print('n: ' + str(n))
+        sift_down(n-1, data, swaps)
+        n -= 1
+    return swaps
+
+
+def main():
+    n = int(input())
+    data = list(map(int, input().split()))
+    assert len(data) == n
+
+    swaps = build_heap(data)
+
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+
+
+if __name__ == "__main__":
+    main()
