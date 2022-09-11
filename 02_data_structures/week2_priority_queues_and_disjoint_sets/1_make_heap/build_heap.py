c0986abacb2e00ecf7a605fc91624b426e125780
yffu <yffu@umich.edu>
2022-09-11 13:42:16 -0700

    week2 problem1 cleanup

--- 02_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ 02_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -27,12 +27,6 @@ def build_heap(data):
 
     Returns a sequence of swaps performed by the algorithm.
     """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
     swaps = []
     n = len(data)//2
 
